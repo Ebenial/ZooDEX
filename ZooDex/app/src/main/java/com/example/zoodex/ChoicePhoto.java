42a171cc3901c34f9fa302c7f05bbdb68c15da17
Ebenial <ebenial22@gmail.com>
2022-05-12 10:39:47 +0200

    "Ajout de la classe ChoicePhoto"

---
+++ ZooDex/app/src/main/java/com/example/zoodex/ChoicePhoto.java
@@ -0,0 +1,28 @@
+package com.example.zoodex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class ChoicePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choicephoto_activity);
+
+        Button button = (Button) findViewById(R.id.photo);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
