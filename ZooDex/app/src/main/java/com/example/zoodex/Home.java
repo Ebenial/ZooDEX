1993050a227141b10d18b1fec1156a92f2eb1482
Samuel Josse <samueljosse3553@gmail.com>
2022-05-12 09:53:40 +0200

    home interface

--- ZooDex/app/src/main/java/com/example/zoodex/Home.java
+++ ZooDex/app/src/main/java/com/example/zoodex/Home.java
@@ -1,5 +1,9 @@
 package com.example.zoodex;
 
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +11,18 @@ public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        Button button = (Button) findViewById(R.id.photo);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
+
 }
