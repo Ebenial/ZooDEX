cde46dccacd39618aebb9ff0991a304cb87cba39
Ebenial <ebenial22@gmail.com>
2022-05-12 11:10:49 +0200

    "Accès à la classe ChoicePhoto après prise de photo"

--- ZooDex/app/src/main/java/com/example/zoodex/HomeActivity.java
+++ ZooDex/app/src/main/java/com/example/zoodex/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.example.zoodex;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,14 +18,25 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-        Button button = (Button) findViewById(R.id.photo);
+        Button photo = (Button) findViewById(R.id.photo);
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+        photo.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(intent, 1);
+            }else{
+                Toast.makeText(HomeActivity.this, "Impossible de prendre une photo, aucune application ne le permet", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            setContentView(R.layout.choicephoto_activity);
+        }
     }
 }
