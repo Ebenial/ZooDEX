947100cb1835173d1ac13d8d3ab9988f151e578a
Ebenial <ebenial22@gmail.com>
2022-05-12 12:43:30 +0200

    Ajout des liens pour accéder aux autres activités

--- ZooDex/app/src/main/java/com/example/zoodex/ChoicePhoto.java
+++ ZooDex/app/src/main/java/com/example/zoodex/ChoicePhoto.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ChoicePhoto extends AppCompatActivity {
@@ -15,11 +16,17 @@ public class ChoicePhoto extends AppCompatActivity {
         setContentView(R.layout.choicephoto_activity);
 
         ImageView image = findViewById(R.id.photo);
+        Button bestiaire = findViewById(R.id.zoodexButton);
 
         //On récupère la photo prise et on l'affiche dans la zone prévue
         Bundle extras = getIntent().getExtras();
         byte[] byteArray = extras.getByteArray("photo");
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         image.setImageBitmap(bmp);
+
+        //Action du bouton "Chercher dans le zoodex", qui amène sur la liste des animaux non découverts pour sélectionner l'animal correspondant
+        bestiaire.setOnClickListener(view -> {
+            //setContentView(R.layout.dexlist_activity);
+        });
     }
 }
