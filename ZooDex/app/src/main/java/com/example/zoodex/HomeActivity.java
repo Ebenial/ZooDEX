0bb83fc88e8fd42bdcedec5702bb4f9a3478e1a9
Samuel Josse <samueljosse3553@gmail.com>
2022-05-12 10:43:39 +0200

    manifest

--- ZooDex/app/src/main/java/com/example/zoodex/HomeActivity.java
+++ ZooDex/app/src/main/java/com/example/zoodex/HomeActivity.java
@@ -1,19 +1,19 @@
 package com.example.zoodex;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.home_activity);
         Button button = (Button) findViewById(R.id.photo);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
