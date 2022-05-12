e1170e6fe46758174aa2e27335c7032ce7a92433
luciaLebrun <lebrunlucia@gmail.com>
2022-05-12 11:59:02 +0200

    ajout photo

--- ZooDex/app/src/main/java/com/example/zoodex/AnimalInfoController.java
+++ ZooDex/app/src/main/java/com/example/zoodex/AnimalInfoController.java
@@ -1,13 +1,18 @@
 package com.example.zoodex;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 import android.widget.VideoView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AnimalInfoController extends AppCompatActivity {
@@ -16,7 +21,6 @@ public class AnimalInfoController extends AppCompatActivity {
     private TextView title;
     private TextView desc;
     private Toolbar navBar;
-    private VideoView photoView;
     private MediaPlayer mediaPlayer;
 
     AnimalInfoController(Bundle savedInstanceState/*Animal animal*/){
@@ -27,7 +31,6 @@ public class AnimalInfoController extends AppCompatActivity {
         this.title = findViewById(R.id.titleAnimal);
         this.desc = findViewById(R.id.descAnimal);
         this.navBar = findViewById(R.id.navBar);
-        this.photoView = findViewById(R.id.photoView);
         //this.mediaPlayer = MediaPlayer.create(this, animal.getSound());
 
         /*this.animalPicture.setBackgroundResource(animal.getImageSource());
@@ -37,8 +40,26 @@ public class AnimalInfoController extends AppCompatActivity {
     }
 
     public void reTakePhoto(View v){
-        photoView.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }else{
+            Toast.makeText(this, "Impossible de prendre une photo, aucune application ne le permet", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            this.animalPicture.setImageBitmap(finalPhoto);
+            setContentView(R.layout.animal_info_activity);
+        }
+    }
+
+
 
     public void sound(View v){
         mediaPlayer.start();
