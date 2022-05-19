bc863c0bb58f4124e2446902d767d6823355f600
yoanndw <yoanndewilde@gmail.com>
2022-05-19 10:52:56 +0200

    [Mon Zoodex] Filter all/found/not found animals

--- ZooDex/app/src/main/java/com/example/zoodex/MyZoodexActivity.java
+++ ZooDex/app/src/main/java/com/example/zoodex/MyZoodexActivity.java
@@ -2,6 +2,8 @@ package com.example.zoodex;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,7 +15,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyZoodexActivity extends Activity {
-    private ArrayList<Animal> animals;
+    private ArrayList<Animal> allAnimals;
+    private ArrayList<Animal> selectedAnimals;
+
     private ZoodexAdapter adapter;
 
     @Override
@@ -21,14 +25,15 @@ public class MyZoodexActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_zoodex_activity);
 
-        this.animals = new ArrayList(Arrays.asList(
+        this.allAnimals = new ArrayList<Animal>(Arrays.asList(
                 new Animal(0, "Capybara", "Desc capy", null, null),
                 new Animal(1, "Lion", "Desc lion", null, null, true),
                 new Animal(2, "Dauphin", "Desc dauphin", null, null),
-                new Animal(3, "Girafe", "Desc girafe", null, null, true)
+                new Animal(3, "Girafe", "Desc girafe", null, null)
         ));
+        this.selectedAnimals = new ArrayList<>(this.allAnimals);
 
-        this.adapter = new ZoodexAdapter(this, R.layout.dex_animal_element, this.animals);
+        this.adapter = new ZoodexAdapter(this, R.layout.dex_animal_element, this.selectedAnimals);
 
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -36,5 +41,46 @@ public class MyZoodexActivity extends Activity {
         RecyclerView gridAnimals = findViewById(R.id.gridAnimals);
         gridAnimals.setLayoutManager(layoutManager);
         gridAnimals.setAdapter(this.adapter);
+
+        // Buttons
+        RadioButton rdShowAll = findViewById(R.id.rdShowAll);
+        rdShowAll.setOnClickListener(e -> this.showAll());
+
+        RadioButton rdShowFound = findViewById(R.id.rdShowFound);
+        rdShowFound.setOnClickListener(e -> this.showFound());
+
+        RadioButton rdShowNotFound = findViewById(R.id.rdShowNotFound);
+        rdShowNotFound.setOnClickListener(e -> this.showNotFound());
+    }
+
+    private void showAll() {
+        this.selectedAnimals.clear();
+        for (Animal a : this.allAnimals) {
+            this.selectedAnimals.add(a);
+        }
+
+        this.adapter.notifyDataSetChanged();
+    }
+
+    private void showFound() {
+        this.selectedAnimals.clear();
+        for (Animal a : this.allAnimals) {
+            if (a.isFound()) {
+                this.selectedAnimals.add(a);
+            }
+        }
+
+        this.adapter.notifyDataSetChanged();
+    }
+
+    private void showNotFound() {
+        this.selectedAnimals.clear();
+        for (Animal a : this.allAnimals) {
+            if (!a.isFound()) {
+                this.selectedAnimals.add(a);
+            }
+        }
+
+        this.adapter.notifyDataSetChanged();
     }
 }
