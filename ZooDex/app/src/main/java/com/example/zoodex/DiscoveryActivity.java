8997081148aabe3366b4707bb123c4494ab3119a
Ebenial <ebenial22@gmail.com>
2022-05-19 09:52:37 +0200

    Tout bug

--- ZooDex/app/src/main/java/com/example/zoodex/DiscoveryActivity.java
+++ ZooDex/app/src/main/java/com/example/zoodex/DiscoveryActivity.java
@@ -1,24 +1,24 @@
 package com.example.zoodex;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
 
 public class DiscoveryActivity extends AppCompatActivity {
+
+
+    @SuppressLint("RestrictedApi")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.discovery_activity);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
-
+        Button locate = (Button) findViewById(R.id.locateToto);
+        locate.setOnClickListener(view -> {
+            setContentView(R.layout.activity_options);
+        });
     }
 
 
